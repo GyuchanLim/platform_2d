e8809044c84e1c379a23fa821898403a10eb2182
Gyuchan Lim <86847942+GyuchanLim@users.noreply.github.com>
2024-10-10 23:14:59 +1300

    some things about different button presses, separating functions for easier visibility, displaying the cards dealt
    Was going to do - add the total card value drawn and display accordingly

--- Scenes/play.gd
+++ Scenes/play.gd
@@ -1,13 +1,47 @@
 extends Node2D
+var deck_to_play = null
+var is_player_ready = false
+var total = 0
+
+@onready var cardsDisplayedLabel = $cardsDisplayed
+@onready var cardsTotalLabel = $cardsTotal
 
 func _ready() -> void:
-	get_node("Play Button/Deal Button").hide()
-	get_node("Play Button/Stop Button").hide()
-	pass
+	get_node("dealButton").hide()
+	get_node("stopButton").hide()
+	get_node("cardsDisplayed").hide()
 
+# BUTTON PRESSES
+func _on_play_button_pressed() -> void:
+	if !is_player_ready:
+		player_ready()
+		prepare_deck()
+	else:
+		print("Player is already ready!")
+
+func _on_deal_button_pressed() -> void:
+	var dealt_card = deck_to_play.pop_back()
+	
+	display_cards(dealt_card)
+	add_to_total(dealt_cards)
+
+func _on_stop_button_pressed() -> void:
+	reset_total()
+	print("Stop button pressed")
+
+# _on_play_button_pressed functions
+func player_ready() -> void:
+	is_player_ready = true
+	get_node("dealButton").show()
+	get_node("stopButton").show()
+	get_node("playButton").hide()
+	get_node("cardsDisplayed").show()
+func prepare_deck():
+	deck_to_play = create_deck(8)
+	deck_to_play.shuffle()
 func create_deck(number_of_decks):
-	const numbers = ['a',1,2,3,4,5,6,7,8,9,10,'j','q','k']
-	const suits = ['s','c','d','h']
+	const numbers = ["a","1","2","3","4","5","6","7","8","9","10","j","q","k"]
+	const suits = ["s","c","d","h"]
 	
 	var deck_to_play = []
 	var deck_of_cards = []
@@ -21,18 +55,18 @@ func create_deck(number_of_decks):
 
 	return deck_to_play
 
-func player_ready() -> void:
-	get_node('Play Button/Deal Button').show()
-	get_node('Play Button/Stop Button').show()
+# _on_deal_button_pressed functions
+func display_cards(card):
+	if cardsDisplayedLabel.text == "":
+		cardsDisplayedLabel.text += card[1]
+	else:
+		cardsDisplayedLabel.text += ", " + card[1]
 
-func _on_play_button_pressed() -> void:
-	var deck_to_play = create_deck(8)
-	player_ready()
+func add_to_total(card):
+	var total = 0
 	
-	# Deal cards
-	# Player first, then dealer
+	cardsTotalLabel.text = total
 
-
-	# Show player options
-	
-	pass # Replace with function body.
+# _on_stop_button_pressed functions
+func reset_count():
+	total = 0
